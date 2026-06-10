ffb58123f2988f36c12bb1e600b9d8d2dbff8b50
AlencarKlaeski <bluewatson2019@gmail.com>
2026-06-10 06:26:09 -0300

    Update script.js
    Hoy practique cadenas y concatenar

---
+++ script.js
@@ -0,0 +1,14 @@
+let lopuedolograr= "si" ;
+let lolograre= "claro que si" ;
+
+let resumen= lopuedolograr + "" + lolograre ;
+console.log( resumen ) ;
+
+let texto="hola"
+let texto2="mundo"
+
+let resultado= texto.concat( "", texto2 ) ;
+console.log( resultado ) ;
+
+let nombre= "Carl" ;
+nombre += "Jhonson" ;
