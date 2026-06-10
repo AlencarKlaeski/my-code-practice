c16876113010789c5f69ae8cd07afcfa0d506c02
AlencarKlaeski <bluewatson2019@gmail.com>
2026-06-10 20:47:38 -0300

    Practica de comentarios

--- script.js
+++ script.js
@@ -12,3 +12,7 @@ console.log( resultado ) ;
 
 let nombre= "Carl" ;
 nombre += "Jhonson" ;
+
+/*Este es un ejemplo de un comentario de varias lineas.
+Esto es realmente bueno*/
+//Este es un ejemplo de un comentario de una sola linea//
