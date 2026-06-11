64226e040b5da67566f06e691c1c1427a069704d
AlencarKlaeski <bluewatson2019@gmail.com>
2026-06-11 20:48:08 -0300

    Hoy estuve practicando variables, tipos de variables

---
+++ practic-text/script.js
@@ -0,0 +1,33 @@
+let adjective = "big";
+
+let noun = "Dragon";
+
+let verb = "cold";
+
+let place = "castle";
+
+let adjective2 = "tooBig";
+
+let noun2 = "Kignigth";
+
+
+const firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+console.log("First story: " + firstStory);
+
+adjective = "kignigth";
+
+noun = "small";
+
+verb = "hot";
+
+place = "city";
+
+adjective2 = "tooSmall";
+
+noun2 = "pears";
+
+const secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+console.log("Second story: " + secondStory);
+
+let type = "52";
+console.log( typeof type );
